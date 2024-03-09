ed97f7c4d7b29a02236e960645a702c98cb99b85
Ahmed Mustafa <Ahmedzakho925@gmail.com>
2024-03-09 13:01:59 +0300

    make design responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import { Avatar, Intro, SkillList } from "./components.jsx";
 
 function App() {
   return (
-    <div className="card">
-      <Avatar />
-      <div className="data">
-        <Intro />
-        <SkillList />
+    <>
+      <header>
+        <h1>Profile Card</h1>
+        <p>
+          this is the profile card of ahmed mustafa, a self taught web developer
+        </p>
+      </header>
+      <div className="card">
+        <Avatar />
+        <div className="data">
+          <Intro />
+          <SkillList />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
